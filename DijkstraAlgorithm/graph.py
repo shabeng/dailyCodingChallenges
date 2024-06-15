079625726bbc052c26162abcd27b09ad620ec1b0
Shabenga <shachafbengal@gmail.com>
2024-06-15 19:51:03 +0300

    changed the nodes elemnts of the graph class

--- DijkstraAlgorithm/graph.py
+++ DijkstraAlgorithm/graph.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 class Graph:
     def __init__(self, edges_list, is_directed=False):
         self.graph = defaultdict(dict)
+        self.nodes = []
         self.create_nodes_edges(edges_list, is_directed)
 
     def __repr__(self):
@@ -15,10 +16,20 @@ class Graph:
             self.graph[node1][node2] = weight
             if not is_directed:
                 self.graph[node2][node1] = weight
+        self.nodes += list(self.graph.keys())
         return
 
+    def is_node(self, node):
+        if not self.graph.get(node, None):
+            return False
+        else:
+            return True
+
+    def get_nodes_list(self):
+        return self.nodes
+
     def get_node_neighbors(self, node_name):
-        return self.graph[node_name]
+        return self.graph[node_name].keys()
 
     def get_edge_weight(self, from_node, to_node):
         w = self.graph[from_node][to_node]
